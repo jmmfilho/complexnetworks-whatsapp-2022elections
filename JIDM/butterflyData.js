86cfba72d766b056524166b610c338237bbc7c17
Fernanda <ffnascimento@sfiec.org.br>
2026-02-14 17:06:00 -0300

    add experimentos JIDM

---
+++ JIDM/butterflyData.js
@@ -0,0 +1,86 @@
+export const butterflyData = {
+  "p1": [
+    {
+      "label": "BANDIDOS",
+      "value": 231
+    },
+    {
+      "label": "LADR\u00c3O",
+      "value": 206
+    },
+    {
+      "label": "BANDIDO",
+      "value": 181
+    },
+    {
+      "label": "BRASILIA",
+      "value": 161
+    },
+    {
+      "label": "COMPROMISSOS",
+      "value": 113
+    },
+    {
+      "label": "FRAUDES",
+      "value": 97
+    },
+    {
+      "label": "URGENTE",
+      "value": 96
+    },
+    {
+      "label": "CURSOS",
+      "value": 79
+    },
+    {
+      "label": "142",
+      "value": 77
+    },
+    {
+      "label": "PTSP",
+      "value": 76
+    }
+  ],
+  "p2": [
+    {
+      "label": "FRAUDE",
+      "value": 554
+    },
+    {
+      "label": "EXPRESIDI\u00c1RIO",
+      "value": 400
+    },
+    {
+      "label": "RATON",
+      "value": 333
+    },
+    {
+      "label": "PATRIOTA",
+      "value": 202
+    },
+    {
+      "label": "LADR\u00c3O",
+      "value": 187
+    },
+    {
+      "label": "PATRIOTAS",
+      "value": 180
+    },
+    {
+      "label": "STM",
+      "value": 177
+    },
+    {
+      "label": "CORRUPTOS",
+      "value": 161
+    },
+    {
+      "label": "FRAUDES",
+      "value": 158
+    },
+    {
+      "label": "BANDIDO",
+      "value": 157
+    }
+  ]
+};
